a3aea937bc5fc06ac9486e5576d8a62a4cfb83b8
Haris Khan <hk@hariskhan.dev>
2024-11-24 17:38:55 -0500

    tracker test

---
+++ lib/tracker/tracker_test.go
@@ -0,0 +1,87 @@
+package tracker
+
+import (
+	"eeptorrent/lib/i2p"
+	"github.com/go-i2p/go-i2p-bt/metainfo"
+	"github.com/sirupsen/logrus"
+	"testing"
+	"time"
+)
+
+func TestTrackers(t *testing.T) {
+	// Setup logging
+	logrus.SetLevel(logrus.DebugLevel)
+
+	mi, err := metainfo.LoadFromFile("/tmp/test.torrent")
+	if err != nil {
+		t.Fatalf("Failed to load test torrent: %v", err)
+	}
+
+	// Initialize SAM connection
+	err = i2p.InitSAM()
+	if err != nil {
+		t.Fatalf("Failed to initialize SAM: %v", err)
+	}
+	defer i2p.CloseSAM()
+
+	// Define test cases
+	trackerTests := []struct {
+		name    string
+		getFn   func(*metainfo.MetaInfo) ([][]byte, error)
+		timeout time.Duration
+	}{
+		{
+			name:    "PostmanTracker",
+			getFn:   GetPeersFromPostmanTracker,
+			timeout: 30 * time.Second,
+		},
+		{
+			name:    "SimpTracker",
+			getFn:   GetPeersFromSimpTracker,
+			timeout: 30 * time.Second,
+		},
+		{
+			name:    "Dg2Tracker",
+			getFn:   GetPeersFromDg2Tracker,
+			timeout: 30 * time.Second,
+		},
+		{
+			name:    "SkankTracker",
+			getFn:   GetPeersFromSkankTracker,
+			timeout: 30 * time.Second,
+		},
+	}
+
+	for _, tt := range trackerTests {
+		t.Run(tt.name, func(t *testing.T) {
+			done := make(chan bool)
+			var peers [][]byte
+			var testErr error
+
+			go func() {
+				peers, testErr = tt.getFn(&mi)
+				done <- true
+			}()
+
+			select {
+			case <-done:
+				if testErr != nil {
+					t.Errorf("Failed to get peers from %s: %v", tt.name, testErr)
+				} else if len(peers) == 0 {
+					t.Errorf("%s returned no peers", tt.name)
+				} else {
+					t.Logf("%s successfully returned %d peers", tt.name, len(peers))
+
+					for i := 0; i < min(3, len(peers)); i++ {
+						t.Logf("Peer %d hash: %x", i, peers[i])
+					}
+				}
+			case <-time.After(tt.timeout):
+				t.Errorf("%s test timed out after %v", tt.name, tt.timeout)
+			}
+		})
+
+		// Add delay between tracker tests to avoid overwhelming I2P
+		time.Sleep(2 * time.Second)
+	}
+}
